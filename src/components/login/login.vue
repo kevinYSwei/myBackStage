2dcbe1dc8fe816c32521b233af0b0682795aa15b
kevinYSwei <Ysw_kevin@163.com>
2019-06-08 16:37:30 +0800

    使用async和await简化login代码

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -29,9 +29,28 @@ export default {
     };
   },
   methods: {
-    handleLogin() {
+      //找到距离异步操作有结果的代码最近的方法，前面加async
+    async handleLogin() {
+      //ES7 异步方法写法  async + await  虽然是异步操作 但是代码执行等同于同步,这种写法对代码性能没有优化 只是看起来更好看些。。
+      //首先找到异步操作有结果的代码前面加上 await 同时接收异步操作结果res
+      const res = await this.$http.post("login", this.loginData);//也就是说这行异步操作有结果后 才会执行后面的代码
+      //当登录成功 跳转home页、提示登录成功
+      //这里是ES6对象解构赋值
+      const {
+        data,
+        meta: { msg, status }
+      } = res.data;
+      if (status === 200) {
+        this.$router.push({
+          name: "home"
+        });
+        this.$message.success(msg);
+      } else {
+        //当登录失败 不跳转、提示登录失败
+        this.$message.warning(msg);
+      }
+      /* //ES6 异步方法写法
       this.$http.post("login", this.loginData).then(res => {
-        console.log(res, 111);
         //当登录成功 跳转home页、提示登录成功
         //这里是ES6对象解构赋值
         const {
@@ -47,7 +66,7 @@ export default {
           //当登录失败 不跳转、提示登录失败
           this.$message.warning(msg);
         }
-      });
+      }); */
     }
   }
 };
