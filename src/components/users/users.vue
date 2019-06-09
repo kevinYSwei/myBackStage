c698cb66d9cd026960292696d4e50719e5a87b91
kevinYSwei <Ysw_kevin@163.com>
2019-06-09 21:42:24 +0800

    完成添加-删除users

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -10,14 +10,21 @@
     <!-- 搜索框 -->
     <el-row class="userSearchWrap">
       <el-col>
-        <el-input placeholder="请输入内容" v-model="userSearch" class="usersInput">
-          <el-button slot="append" icon="el-icon-search"></el-button>
+        <!-- 当清空搜索框时 数据全部重新加载 -->
+        <el-input
+          @clear="loadUserList"
+          clearable
+          placeholder="请输入内容"
+          v-model="userSearch"
+          class="usersInput"
+        >
+          <el-button @click="searchKeys" slot="append" icon="el-icon-search"></el-button>
         </el-input>
-        <el-button type="success" plain>添加用户</el-button>
+        <el-button type="success" plain @click="addUserDialog">添加用户</el-button>
       </el-col>
     </el-row>
-    <!-- 表格 el-table(data数据源[]) el-table-column 列 (label表头 prop="每个对象的属性数据") -->
-    <el-table :data="userList" style="width: 100%">
+    <!-- 表格 el-table(data数据源[]) el-table-column 列 (label表头 prop="每个对象的属性数据") 给表格设置固定高度  将自动在数据超出时出现滚动条 并且固定表头-->
+    <el-table :data="userList" style="width: 100%" height="400px">
       <el-table-column type="index" label="#" width="60"></el-table-column>
       <el-table-column prop="username" label="姓名" width="80"></el-table-column>
       <el-table-column prop="email" label="邮箱"></el-table-column>
@@ -27,7 +34,7 @@
         <!-- 若template内部要用到外层组件的数据 则需要设置slot-scope属性 
         注意： 该属性的值是要用的create_time的数据源 【userList】-->
         <!-- 不同组件的作用域不同 数据不共享 这里用到插槽传值 -->
-        <!-- slot-scope 作用：传值，slot-scope的值会自动去上一级寻找所需数据的数据源，这里是el-table绑定的数据userList，并将上一层数据源赋值给自定义名(scope)，此时"scope" == userList数组  故属性值名userList是任意取的 -->
+        <!-- slot-scope 作用：传值，slot-scope的值会自动去上一级寻找所需数据的数据源，这里是el-table绑定的数据userList，并将上一层数据源赋值给自定义名(scope)，此时"scope" == userList数组  故属性值名userList可以是任意取的 -->
         <!-- 且userList.row指数组中的每一个对象 -->
         <!-- <template slot-scope="userList"> -->
         <template slot-scope="scope">
@@ -43,8 +50,22 @@
       </el-table-column>
       <el-table-column prop="address" label="操作">
         <template slot-scope="scope">
-          <el-button size="mini" plain type="primary" icon="el-icon-edit" circle></el-button>
-          <el-button size="mini" plain type="danger" icon="el-icon-delete" circle></el-button>
+          <el-button
+            size="mini"
+            plain
+            type="primary"
+            icon="el-icon-edit"
+            @click="showEditUserMsgBox"
+            circle
+          ></el-button>
+          <el-button
+            size="mini"
+            plain
+            type="danger"
+            icon="el-icon-delete"
+            @click="showDelUserMsgBox(scope.row.id)"
+            circle
+          ></el-button>
           <el-button size="mini" plain type="success" icon="el-icon-check" circle></el-button>
         </template>
       </el-table-column>
@@ -59,19 +80,69 @@
       layout="total, sizes, prev, pager, next, jumper"
       :total="total"
     ></el-pagination>
+    <!-- 添加用户 dialog -->
+    <el-dialog title="添加用户" :visible.sync="dialogFormVisibleAdd">
+      <el-form :model="form">
+        <el-form-item label="用户名" label-width="100px">
+          <el-input v-model="form.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密 码" label-width="100px">
+          <el-input v-model="form.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="100px">
+          <el-input v-model="form.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width="100px">
+          <el-input v-model="form.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="cancelAdd">取 消</el-button>
+        <el-button type="primary" @click="confirmAdd">确 定</el-button>
+      </div>
+      <!-- form里有哪些数据 看接口文档添加用户需要哪些数据 username password email mobile -->
+    </el-dialog>
+    <!-- 编辑用户信息 dialog -->
+    <el-dialog title="编辑用户" :visible.sync="dialogFormVisibleEdit">
+      <el-form :model="form">
+        <el-form-item label="用户名" label-width="100px">
+          <el-input v-model="form.username" autocomplete="off" disabled></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" label-width="100px">
+          <el-input v-model="form.email" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话" label-width="100px">
+          <el-input v-model="form.mobile" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisibleEdit = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisibleEdit = false">确 定</el-button>
+      </div>
+      <!-- form里有哪些数据 看接口文档添加用户需要哪些数据 username password email mobile -->
+    </el-dialog>
   </el-card>
 </template>
 <script>
 export default {
   data() {
     return {
-      userSearch: "",
+      userSearch: "", //双向数据绑定
       //表格绑定数据
       userList: [],
       //分页相关数据
       total: -1, //总数据条数
       pagenum: 1, //当前页
-      pagesize: 4 //每页显示条数
+      pagesize: 4, //每页显示条数,
+      //添加用户dialog
+      dialogFormVisibleAdd: false,
+      dialogFormVisibleEdit: false,
+      form: {
+        username: "",
+        password: "",
+        email: "",
+        mobile: ""
+      }
     };
   },
   created() {
@@ -79,18 +150,90 @@ export default {
     this.getUserList();
   },
   methods: {
+    //编辑用户
+    showEditUserMsgBox() {
+        this.dialogFormVisibleEdit = true
+    },
+    //点击删除小图标
+    showDelUserMsgBox(userid) {
+      this.$confirm("此操作将永久删除该文件, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(async () => {
+          //点击确定执行  这里后台提供的方法接口是 delete()
+          const res = await this.$http.delete(`users/${userid}`);
+          console.log(res, 222);
+          if (res.data.meta.status === 200) {
+            this.pagenum = 1;
+            this.$message({
+              type: "success",
+              message: res.data.meta.msg
+            });
+            this.$nextTick(() => {
+              this.getUserList(); //删除成功后重新更新视图
+            });
+          }
+        })
+        .catch(() => {
+          //点击取消执行
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+    //取消添加数据
+    cancelAdd() {
+      this.dialogFormVisibleAdd = false;
+      this.form = {};
+    },
+    //确认添加数据
+    async confirmAdd() {
+      this.dialogFormVisibleAdd = false;
+      const res = await this.$http.post("users", this.form);
+      console.log(res, 343);
+      const {
+        data,
+        meta: { msg, status }
+      } = res.data;
+      if (status === 201) {
+        this.$message.success(msg);
+        this.form = {};
+        this.getUserList(); //更新视图
+      } else {
+        this.$message.warning(msg);
+        this.form = {};
+      }
+    },
+    //添加用户--显示dialog
+    addUserDialog() {
+      this.dialogFormVisibleAdd = true;
+    },
+    //点击清空搜索框按钮 触发
+    loadUserList() {
+      this.getUserList();
+    },
+    //搜索关键字按钮
+    searchKeys() {
+      this.getUserList();
+    },
     //分页操作
-    handleSizeChange(val) { //改变每页显示条数
+    handleSizeChange(val) {
+      //改变每页显示条数
       console.log(`每页 ${val} 条`);
       this.pagesize = val;
       //并且无论原先页码是多少 只要切换页内数据量后 都让他从第一页开始展示
       this.pagenum = 1;
       //此方法触发时 要重新从后台获取数据
-      this.$nextTick(()=>{ //延迟加载，在将DOM结构重新加载完成以后再触发
+      this.$nextTick(() => {
+        //延迟加载，在将DOM结构重新加载完成以后再触发
         this.getUserList();
-      })
+      });
     },
-    handleCurrentChange(val) { //改变页码
+    handleCurrentChange(val) {
+      //改变页码
       console.log(`当前页: ${val}`);
       this.pagenum = val;
       //此方法触发时 要重新从后台获取数据
@@ -106,6 +249,7 @@ export default {
       const AUTH_TOKEN = localStorage.getItem("token");
       this.$http.defaults.headers.common["Authorization"] = AUTH_TOKEN;
       const res = await this.$http.get(
+        //当query为空时 请求加载全部数据
         `users?query=${this.userSearch}&pagenum=${this.pagenum}&pagesize=${
           this.pagesize
         }`
@@ -125,9 +269,9 @@ export default {
       if (status === 200) {
         this.userList = users;
         this.total = total;
-        this.$message.success(msg);
+        // this.$message.success(msg);
       } else {
-        this.$message.warning(msg);
+        // this.$message.warning(msg);
       }
     }
   }
@@ -143,5 +287,10 @@ export default {
 }
 .usersInput {
   width: 300px;
+}
+.box-card .el-dialog__title {
+  display: inline-block;
+  font: bold 24px/40px "微软雅黑";
+  padding-left: 20px;
 }
 </style>
