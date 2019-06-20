d56ac881a03e5c8d52b41bd02fe349551239c91e
kevinYSwei <Ysw_kevin@163.com>
2019-06-20 20:20:07 +0800

    实现打包

--- build/utils.js
+++ build/utils.js
@@ -47,7 +47,9 @@ exports.cssLoaders = function (options) {
     if (options.extract) {
       return ExtractTextPlugin.extract({
         use: loaders,
-        fallback: 'vue-style-loader'
+        fallback: 'vue-style-loader',
+        //这里解决 打包后图标显示不出来问题
+        publicPath: '../../'
       })
     } else {
       return ['vue-style-loader'].concat(loaders)
